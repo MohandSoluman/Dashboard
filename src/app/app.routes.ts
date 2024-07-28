74af3d9c899f17f4f36a2a31b9778ac729051dd8
MohandSoluman <mohandsoluman@gmail.com>
2024-07-28 14:26:52 +0300

    add new functionality

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,24 @@
 import { Routes } from '@angular/router';
 import { BankListComponent } from './routes/bank-definition/bank-list/bank-list.component';
 import { BankFormComponent } from './routes/bank-definition/bank-form/bank-form.component';
+import { UserListComponent } from './routes/user-list/user-list.component';
+import { UserManagmentComponent } from './routes/user-managment/user-managment.component';
 
 export const routes: Routes = [
   { path: 'bankList', component: BankListComponent },
   { path: 'bankForm', component: BankFormComponent },
-  { path: '', redirectTo: '/bankList', pathMatch: 'full' },
+  {
+    path: 'users',
+    component: UserListComponent,
+  },
+  {
+    path: 'user-management',
+    component: UserManagmentComponent,
+  },
+  {
+    path: 'user-management/:id',
+    component: UserManagmentComponent,
+  },
+  { path: '', redirectTo: '/users', pathMatch: 'full' },
+  { path: '**', redirectTo: '/users' },
 ];
