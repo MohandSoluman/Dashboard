78f77d367c1cc3a0e35afbb0568491ac2ce5e68d
MohandSoluman <mohandsoluman@gmail.com>
2024-07-28 15:14:04 +0300

    remove unneccessary review code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,19 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './layout/header/header.component';
 import { SideNavbarComponent } from './layout/side-navbar/side-navbar.component';
-import { BankListComponent } from './routes/bank-definition/bank-list/bank-list.component';
-import { BankFormComponent } from './routes/bank-definition/bank-form/bank-form.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    RouterOutlet,
-    HeaderComponent,
-    SideNavbarComponent,
-    BankListComponent,
-    BankFormComponent,
-  ],
+  imports: [RouterOutlet, HeaderComponent, SideNavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,8 @@
 import { Routes } from '@angular/router';
-import { BankListComponent } from './routes/bank-definition/bank-list/bank-list.component';
-import { BankFormComponent } from './routes/bank-definition/bank-form/bank-form.component';
 import { UserListComponent } from './routes/user-list/user-list.component';
 import { UserManagmentComponent } from './routes/user-managment/user-managment.component';
 
 export const routes: Routes = [
-  { path: 'bankList', component: BankListComponent },
-  { path: 'bankForm', component: BankFormComponent },
   {
     path: 'users',
     component: UserListComponent,
